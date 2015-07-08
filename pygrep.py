fed1078fd25fd4b88db7f4c76830015bb2d5f202
n.lyubchich <cybran111@gmail.com>
2015-07-08 18:03:49 +0300

    Grep is somewhat splitted

--- pygrep.py
+++ pygrep.py
@@ -10,25 +10,36 @@ def grep_file(file, keyword):
         if keyword in line:
             yield number, line
 
+def get_filepath(root, filenames):
+    for filename in filenames:
+        yield os.path.join(root, filename)
+
+
+def get_file(filenames):
+    for filename in filenames:
+        yield open(filename), filename
+
+def get_matches(filename_pattern):
+    for root, dirs, files in os.walk(SCRIPT_DIR):
+        # Filtering files by matching a pattern
+        yield filter(lambda x: fnmatch(x, filename_pattern), files)
+
 
 def grep(filename_pattern, keyword):
     for root, dirs, files in os.walk(SCRIPT_DIR):
         # Filtering files by matching a pattern
         matched_files = filter(lambda x: fnmatch(x, filename_pattern), files)
 
-        for filename in matched_files:
-
-            file_fullpath = os.path.join(SCRIPT_DIR, root, filename)
-            with open(file_fullpath) as file:
-                try:
-                    for number, line in grep_file(file, keyword):
-                        print("{}:{}: {}".format(
-                            file_fullpath,
-                            number,
-                            line.rstrip()
-                        ))
-                except UnicodeDecodeError:
-                    print(file_fullpath, " seems to be binary, skipping")
+        for file, filename in get_file(get_filepath(root, matched_files)):
+            try:
+                for number, line in grep_file(file, keyword):
+                    print("{}:{}: {}".format(
+                        filename,
+                        number,
+                        line.rstrip()
+                    ))
+            except UnicodeDecodeError:
+                    print(filename, "seems to be binary, skipping")
 
 
 if __name__ == '__main__':
